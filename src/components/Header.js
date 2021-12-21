bdda36d5390a89727f97a5dd62a1f9ee4b7d867c
Kivi Corn <kivi.webdev@gmail.com>
2021-12-21 20:05:24 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return ( 
         <header>
             <h1>My Favorite Dog Breeds</h1>
-            <a href="/frontend-code-challenge.pdf" target="_blank">View Assignment</a>
+            <a href="https://bykivi.com/dog-breeds/frontend-code-challenge.pdf" target="_blank">View Assignment</a>
         </header>
      );
 }
